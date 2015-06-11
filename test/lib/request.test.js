4f61688b061b0e4065a2b727d920b14a05c6d4c2
Tim Schaub <tim.schaub@gmail.com>
2015-06-11 14:48:00 -0600

    Shorthand for get requests

--- test/lib/request.test.js
+++ test/lib/request.test.js
@@ -14,49 +14,40 @@ var assert = chai.assert;
 
 describe('request', function() {
 
+  var httpRequest = http.request;
+  var httpsRequest = https.request;
+  var mockRequest = null;
+
+  beforeEach(function() {
+    mockRequest = {
+      end: sinon.spy(),
+      abort: sinon.spy()
+    };
+    http.request = sinon.spy(function() {
+      return mockRequest;
+    });
+    https.request = sinon.spy(function() {
+      return mockRequest;
+    });
+  });
+
+  afterEach(function() {
+    http.request = httpRequest;
+    https.request = httpsRequest;
+    mockRequest = null;
+    auth.clear();
+  });
+
   describe('request()', function() {
-    var httpRequest = http.request;
-    var httpsRequest = https.request;
-    var mockRequest = null;
     var request = req.request;
 
-    beforeEach(function() {
-      mockRequest = {
-        end: sinon.spy(),
-        abort: sinon.spy()
-      };
-      http.request = sinon.spy(function() {
-        return mockRequest;
-      });
-      https.request = sinon.spy(function() {
-        return mockRequest;
-      });
-    });
-
-    afterEach(function() {
-      http.request = httpRequest;
-      https.request = httpsRequest;
-      mockRequest = null;
-      auth.clear();
-    });
-
-    it('provides a shorthand for calling http.request()', function() {
-      request('http://example.com');
-      assert.equal(http.request.callCount, 1);
-    });
-
-    it('uses https.request() for https URLs', function() {
-      request('https://example.com');
-      assert.equal(https.request.callCount, 1);
-    });
-
     it('returns a promise', function() {
-      var promise = request('https://example.com');
+      var promise = request({url: 'https://example.com'});
       assert.instanceOf(promise, Promise);
     });
 
     it('calls http.request() with options and callback', function() {
-      request('http://example.com');
+      request({url: 'http://example.com'});
 
       assert.equal(http.request.callCount, 1);
       var call = http.request.getCall(0);
@@ -74,25 +65,9 @@ describe('request', function() {
       assert.typeOf(call.args[1], 'function');
     });
 
-    it('accepts a url string', function() {
-      request({
-        url: 'http://example.com'
-      });
-
-      assert.equal(http.request.callCount, 1);
-      var call = http.request.getCall(0);
-      assert.lengthOf(call.args, 2);
-
-      assert.deepEqual(call.args[0], {
-        protocol: 'http:',
-        hostname: 'example.com',
-        port: '80',
-        method: 'GET',
-        path: '/',
-        headers: {accept: 'application/json'}
-      });
-
-      assert.typeOf(call.args[1], 'function');
+    it('uses https.request() for https URLs', function() {
+      request({url: 'https://example.com'});
+      assert.equal(https.request.callCount, 1);
     });
 
     it('accepts a terminator for aborting requests', function(done) {
@@ -113,13 +88,38 @@ describe('request', function() {
 
   });
 
+  describe('get()', function() {
+
+    it('calls request() with method set to GET', function() {
+      req.get({url: 'http://example.com'});
+      assert.equal(http.request.callCount, 1);
+      var call = http.request.getCall(0);
+      assert.lengthOf(call.args, 2);
+      var config = call.args[0];
+      assert.equal(config.method, 'GET');
+    });
+
+    it('accepts a string for the URL', function() {
+      req.get('http://example.com');
+      assert.equal(http.request.callCount, 1);
+      var call = http.request.getCall(0);
+      assert.lengthOf(call.args, 2);
+      var config = call.args[0];
+      assert.equal(config.method, 'GET');
+      assert.equal(config.hostname, 'example.com');
+    });
+
+  });
+
   describe('parseConfig()', function() {
 
     var parseConfig = req.parseConfig;
     var defaultHeaders = {'accept': 'application/json'};
 
     it('generates request options from a URL', function() {
-      var config = 'http://example.com';
+      var config = {
+        url: 'http://example.com'
+      };
       var options = {
         protocol: 'http:',
         hostname: 'example.com',
@@ -133,7 +133,9 @@ describe('request', function() {
     });
 
     it('uses the correct default port for https', function() {
-      var config = 'https://example.com';
+      var config = {
+        url: 'https://example.com'
+      };
       var options = {
         protocol: 'https:',
         hostname: 'example.com',
@@ -147,7 +149,9 @@ describe('request', function() {
     });
 
     it('respects the port in the URL', function() {
-      var config = 'http://example.com:8000';
+      var config = {
+        url: 'http://example.com:8000'
+      };
       var options = {
         protocol: 'http:',
         hostname: 'example.com',
@@ -161,7 +165,9 @@ describe('request', function() {
     });
 
     it('respects a query string in the URL', function() {
-      var config = 'http://example.com/page?foo=bar';
+      var config = {
+        url: 'http://example.com/page?foo=bar'
+      };
       var options = {
         protocol: 'http:',
         hostname: 'example.com',
