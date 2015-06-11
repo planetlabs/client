5580f7612d2042f3f48952ccb1a4ca3d6822f822
Tim Schaub <tim.schaub@gmail.com>
2015-06-11 13:36:48 -0600

    Configurable URLs

---
+++ test/lib/urls.test.js
@@ -0,0 +1,52 @@
+/* eslint-env mocha */
+
+var assert = require('chai').assert;
+
+var urls = require('../../lib/urls');
+
+describe('urls', function() {
+
+  describe('join()', function() {
+
+    it('joins URL parts', function() {
+      var cases = [{
+        actual: urls.join('http://example.com', 'foo'),
+        expected: 'http://example.com/foo'
+      }, {
+        actual: urls.join('http://example.com', 'foo', 'bar'),
+        expected: 'http://example.com/foo/bar'
+      }, {
+        actual: urls.join('http://example.com', 'foo', 'bar', 'bam'),
+        expected: 'http://example.com/foo/bar/bam'
+      }, {
+        actual: urls.join('http://example.com', 'foo', 'bar', 'bam', ''),
+        expected: 'http://example.com/foo/bar/bam/'
+      }];
+
+      for (var i = 0, ii = cases.length; i < ii; ++i) {
+        var c = cases[i];
+        assert.deepEqual(c.actual, c.expected, 'case ' + i);
+      }
+    });
+
+    it('removes extra slashes', function() {
+      var cases = [{
+        actual: urls.join('http://example.com/', 'foo'),
+        expected: 'http://example.com/foo'
+      }, {
+        actual: urls.join('http://example.com/', '/foo', 'bar'),
+        expected: 'http://example.com/foo/bar'
+      }, {
+        actual: urls.join('http://example.com', 'foo/', '/bar/', 'bam/'),
+        expected: 'http://example.com/foo/bar/bam'
+      }];
+
+      for (var i = 0, ii = cases.length; i < ii; ++i) {
+        var c = cases[i];
+        assert.deepEqual(c.actual, c.expected, 'case ' + i);
+      }
+    });
+
+  });
+
+});
