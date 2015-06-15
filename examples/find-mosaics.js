d374248fb8015b66eae806c74efe567129a2149c
Tim Schaub <tim.schaub@gmail.com>
2015-06-15 08:37:30 -0600

    Page through mosaics

---
+++ examples/find-mosaics.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+
+var planet = require('../lib/index');
+
+planet.auth.setKey(process.env.PL_API_KEY);
+
+var mosaics = [];
+var limit = 500;
+
+function fetch(promise) {
+  return promise.then(function(page) {
+    mosaics = mosaics.concat(page.data.mosaics);
+    console.log('got ' + mosaics.length + ' mosaics');
+    if (page.next && mosaics.length < limit) {
+      return fetch(page.next());
+    }
+  });
+}
+
+fetch(planet.mosaics.find())
+  .then(function() {
+    console.log('done fetching');
+  }).catch(function(err) {
+    console.error('Failed to fetch mosaics:', err.message);
+  });
