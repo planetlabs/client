242e3c5106ebe8a49f2024077e0d98010c53c64e
Tim Schaub <tim.schaub@gmail.com>
2015-06-15 08:28:05 -0600

    Mosaic access

---
+++ examples/get-mosaic.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+
+var planet = require('../lib/index');
+
+planet.auth.setKey(process.env.PL_API_KEY);
+
+planet.mosaics.get('color_balance_mosaic')
+  .then(function(mosaic) {
+    console.log(JSON.stringify(mosaic, null, 2));
+  })
+  .catch(function(err) {
+    console.error('Failed to fetch mosaic:', err.message);
+  });
