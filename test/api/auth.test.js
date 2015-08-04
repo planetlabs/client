812007529433f15a4842b05ade5b0be8437cef9a
Tim Schaub <tim.schaub@gmail.com>
2015-08-04 17:18:39 -0600

    Additional tests for auth.login()

--- test/api/auth.test.js
+++ test/api/auth.test.js
@@ -9,6 +9,7 @@ var sinon = require('sinon');
 
 var auth = require('../../api/auth');
 var authStore = require('../../api/auth-store');
+var errors = require('../../api/errors');
 
 describe('auth', function() {
 
@@ -89,6 +90,51 @@ describe('auth', function() {
       response.emit('end');
     });
 
+    it('rejects if body does not contain a token', function(done) {
+      var response = new stream.Readable();
+      response.statusCode = 200;
+      var body = {foo: 'bar'};
+
+      var email = 'user@email.com';
+      var password = 'psswd';
+      auth.login(email, password).then(function(success) {
+        done(new Error('Expected rejection'));
+      }, function(err) {
+        assert.instanceOf(err, errors.UnexpectedResponse);
+        done();
+      });
+
+      assert.equal(https.request.callCount, 1);
+      var args = https.request.getCall(0).args;
+      assert.lengthOf(args, 2);
+      var callback = args[1];
+      callback(response);
+      response.emit('data', JSON.stringify(body));
+      response.emit('end');
+    });
+
+    it('rejects if body contains a bogus token', function(done) {
+      var response = new stream.Readable();
+      response.statusCode = 200;
+      var body = {token: 'bogus'};
+
+      var email = 'user@email.com';
+      var password = 'psswd';
+      auth.login(email, password).then(function(success) {
+        done(new Error('Expected rejection'));
+      }, function(err) {
+        assert.instanceOf(err, errors.UnexpectedResponse);
+        done();
+      });
+
+      assert.equal(https.request.callCount, 1);
+      var args = https.request.getCall(0).args;
+      assert.lengthOf(args, 2);
+      var callback = args[1];
+      callback(response);
+      response.emit('data', JSON.stringify(body));
+      response.emit('end');
+    });
   });
 
   describe('setKey()', function() {
