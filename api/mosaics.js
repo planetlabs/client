bd52268f04187ee38711ee4084479d546002d862
Tim Schaub <tim.schaub@gmail.com>
2015-08-08 14:52:33 -0500

    Markdown params and returns

--- api/mosaics.js
+++ api/mosaics.js
@@ -17,7 +17,9 @@ var util = require('./util');
  * @param {function(function())} options.terminator A function that is called
  *     with a function that can be called back to terminate the request.
  * @return {Promise.<Object>} A promise that resolves to mosaic metadata or is
- *     rejected with any error.
+ *     rejected with any error.  See the [`errors`
+ *     module](#module:planet-client/api/errors) for a list of the possible
+ *     error types.
  */
 function get(id, options) {
   options = options || {};
@@ -43,6 +45,8 @@ function get(id, options) {
  *     with a function that can be called back to terminate the request.
  * @return {Promise.<module:planet-client/api/page~Page>} A promise that
  *     resolves to a page of mosaic metadata or is rejected with any error.
+ *     See the [`errors` module](#module:planet-client/api/errors) for a list of
+ *     the possible error types.
  */
 function search(query, options) {
   options = options || {};
--- api/scenes.js
+++ api/scenes.js
@@ -20,6 +20,8 @@ var util = require('./util');
  *     with a function that can be called back to terminate the request.
  * @return {Promise.<Object>} A promise that resolves to scene metadata or is
  *     rejected with any error.
+ *     See the [`errors` module](#module:planet-client/api/errors) for a list of
+ *     the possible error types.
  */
 function get(scene, options) {
   options = options || {};
@@ -51,6 +53,8 @@ function get(scene, options) {
  *     with a function that can be called back to terminate the request.
  * @return {Promise.<module:planet-client/api/page~Page>} A promise that
  *     resolves to a page of scene metadata or is rejected with any error.
+ *     See the [`errors` module](#module:planet-client/api/errors) for a list of
+ *     the possible error types.
  */
 function search(query, options) {
   options = options || {};
